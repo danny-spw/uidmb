2138ee9e647510eeb308e820c6eaf87e06abfada
danny <10930278@dcsh.tp.edu.tw>
2022-01-05 11:29:12 +0800

    刪除討論主題、主題計數器頁面

--- topic/views.py
+++ topic/views.py
@@ -2,6 +2,8 @@ from django.views.generic import *
 from django.urls import reverse
 from .models import *
 from datetime import datetime
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+
 
 # 討論主題列表
 class TopicList(ListView):
@@ -10,7 +12,7 @@ class TopicList(ListView):
     paginate_by = 20        # 每頁主題數
 
 # 新增討論主題
-class TopicNew(CreateView):
+class TopicNew(LoginRequiredMixin, CreateView):
     model = Topic
     fields = ['subject', 'content']
 
@@ -32,8 +34,14 @@ class TopicView(DetailView):
         ctx['reply_list'] = Reply.objects.filter(topic=self.object)
         return ctx
 
+    def get_object(self):
+        topic = super().get_object()    # 取得欲查看的討論主題
+        topic.hits += 1     # 等同 topic.hits = topic.hits + 1
+        topic.save()
+        return topic
+
 # 回覆討論主題
-class TopicReply(CreateView):
+class TopicReply(LoginRequiredMixin,CreateView):
     model = Reply
     fields = ['content']
     template_name = 'topic/topic_form.html'
@@ -48,3 +56,22 @@ class TopicReply(CreateView):
 
     def get_success_url(self):
         return reverse('topic_view', args=[self.kwargs['tid']])
+
+# 刪除討論主題
+class TopicDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'topic.delete_topic'
+    model = Topic
+    template_name = 'confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse('topic_list')
+
+# 刪除討論回覆
+class ReplyDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'topic.delete_reply'
+    model = Reply
+    template_name = 'confirm_delete.html'
+
+    def get_success_url(self):
+        reply = self.get_object()   # 取得欲刪除的那筆紀錄
+        return reverse('topic_view', args=[reply.topic.id])
